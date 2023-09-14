89f5fca254c600908241dc8ab927c2e163d7e3fc
paulohmorais <pauloh.morais@hotmail.com>
2023-09-14 16:05:22 -0300

    devopsdays promo and update on homepagefeatures

---
+++ docs/devopsdaysfloripa.md
@@ -0,0 +1,92 @@
+---
+sidebar_position: 2
+sidebar_label: 'Promoção DevOpsDays Florianópolis 2023'
+title: 'Promoção: a vida é dura mas a praia é mole'
+---
+
+<center>
+
+![rawdevopslogo](../static/img/docsImages/devopsdaysfloripa.png)
+
+</center>
+
+<center>
+
+[![ButtonPromo1](../static/img/docsImages/button1.png)](https://www.udemy.com/course/raw-devops-bootcamp/?couponCode=RAWDEVOPSFLN)
+
+</center>
+
+**A RAW DevOps vai levar um dos nossos alunos para o DevOpsDays Florianópolis 2023 que vai acontecer dia 11/11/2023!**
+
+Já pensou ir em um dos eventos DevOpsDays com tudo pago?
+
+Vamos dar essa oportunidade incrível para um dos nossos alunos e que ainda está se graduando em TI e ajuda-los neste momento inicial de suas carreiras. Eventos como este vão te trazer para o que há de mais moderno na área com experiências reais compartilhadas de palestrantes das maiores empresas do país. Uma experiencia que pode mudar a carreira de alguém iniciante e também agregar muito ao currículo.
+
+E não menos importante ainda visitar Florianópolis \o/
+
+Florianópolis é conhecida por ser a "Ilha da Magia", por suas belas praias e seus pontos turísticos, e também por ser a "Ilha do Silício" esse polo cada vez mais em expansão em tecnologia que está cada vez mais atraindo os olhos do mundo.
+
+E o DevOpsDays é uma série de conferências mundial onde os temas são desenvolvimento de software, operação da infraestrutura de TI e a intersecção entre eles. 
+
+Uma oportunidade incrível de network e aprendizado.
+
+E é claro, ainda haverá aquele certificado participação para lhe ajudar com as horas complementares da sua graduação.
+
+E você sabia que o nosso instrutor será um dos palestrantes? Este é um dos motivos que estamos fazendo esta promoção.
+
+Mais informações sobre o evento no link: [**DevOpsDays Florianópolis**](https://devopsdays.org/events/2023-florianopolis/welcome/)
+
+Leia atentamente as informações abaixo e caso ainda persista alguma dúvida nos envie um email para: **rawdevops@gmail.com**
+
+## O que está incluso?
+
+- Passagens aéreas ida e volta para Florianópolis partindo do aeroporto com voo comercial + próximo ao ganhador
+- Voo de ida: 10/11/2023 e Voo de volta: 12/11/2023
+- 2 dias de hospedagem em Hotel próximo ao evento
+- 1 corrida Uber do aeroporto de Florianópolis até o Hotel
+- 1 corrida Uber do Hotel ate o local do evento
+- 1 corrida Uber do local do evento ate o Hotel
+- 1 corrida Uber do Hotel ate o aeroporto
+- Não arcaremos com o translado da sua residência até o aeroporto, nem ao retorno do evento o translado do aeroporto ate a sua residência
+- 2 dias de ajuda de custo no valor de R$ 110,00 por dia
+- Carona para a praia mole com o instrutor se quiser :D
+
+<center>
+
+[![ButtonPromo2](../static/img/docsImages/button2.png)](https://www.udemy.com/course/raw-devops-bootcamp/?couponCode=RAWDEVOPSFLN)
+
+</center>
+
+## Regras
+
+- Ter + de 18 anos
+- Estar cursando algum curso superior ou tecnólogo em TI (ex: Sistemas de Informação, Analise e Desenvolvimento de Sistemas, Ciências da Computação, Engenharia de Software, Gestão da Informação, entre outros)
+- O ganhador deverá apresentar o comprovante de matrícula do atual semestre ou comprovante de vínculo
+- Estar disponível para viajar dia 10/11 e retornar dia 12/11, os voos podem partir de qualquer horário nos dias citados, conforme disponibilidade de voos de cada aeroporto
+- Ter comprado nosso curso utilizando cupom de desconto (Ex: RAWDEVOPSFLN, RAWDEVOPS)
+
+<center>
+
+[![ButtonPromo2](../static/img/docsImages/button2.png)](https://www.udemy.com/course/raw-devops-bootcamp/?couponCode=RAWDEVOPSFLN)
+
+</center>
+
+
+## Como vai funcionar?
+
+- Apenas 200 vagas disponíveis para o sorteio com 1 ganhador
+- **Sorteio no próximo sorteio da loteria federal assim que os números forem preenchidos, que ocorrem as quartas e sábado ou dia 11/10/2023**
+- Após a compra do curso e se você esta dentro dos requisitos das regras, preencher o form da promoção: [**https://forms.gle/KBeB9ZyicTgDqisK9**](https://forms.gle/KBeB9ZyicTgDqisK9)
+- Escolher 5 números disponíveis na planilha: [**https://docs.google.com/spreadsheets/d/1JWW38WaJCuJQttom7q4pU42zrwdjMOBg7btWCr8gpx0/edit?usp=sharing**](https://docs.google.com/spreadsheets/d/1JWW38WaJCuJQttom7q4pU42zrwdjMOBg7btWCr8gpx0/edit?usp=sharing)
+- Após o preenchimento do form, o mais breve possível ou em até 1 dia útil, entraremos em contato para a coleta dos números escolhidos, reservar e confirmar os mesmos.
+- Aguardar a reserva e confirmação dos números escolhidos
+- Apos confirmação dos números escolhidos te adicionaremos a um grupo de Whatsapp com todos os 200 participantes, onde vocês serão atualizados sobre o andamento da promoção e o preenchimento das vagas.
+- Assim que preencher os 200 números vamos fazer uma live para o sorteio no proximo sorteio da loteria federal ou então no dia 11/10/2023 caso não se preencha todos os números
+- O número sorteado será os 3 últimos números do primeiro sorteio/destino do sorteio realizado pela loteria federal
+- Aos participantes vamos sempre mantê-los atualizados no grupo de Whatsapp e desejo boa sorte a todos.
+
+<center>
+
+[![ButtonPromo3](../static/img/docsImages/button3.png)](https://www.udemy.com/course/raw-devops-bootcamp/?couponCode=RAWDEVOPSFLN)
+
+</center>
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,64 +10,138 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Bem-vindo a Gataria!',
-    Svg: require('@site/static/img/homepagefeatures/welcome.svg').default,
+    title: 'CODE',
+    Svg: require('@site/static/img/homepagefeatures/code.svg').default,
     description: (
       <>
-        A nossa jornada começa no primeiro dia de trabalho como o primeiro DevOps para uma nova rede social.
-        Assim precisaremos construir tudo do zero!
-        Uma grande desafio! Porém também uma excelente oportunidade de aprendizado não é?
+        Desembarcamos na Gataria como DevOps! É uma rede social exclusiva para gatos, e aqui estamos no nosso primeiro dia de trabalho.
+
+Primeiro, navegaremos no site para entender o que os devs já construíram. A arquitetura baseada em microserviços nos chamou a atenção - uma escolha inteligente!
+
+E finalmente é hora de colocar a mão na massa! Iniciaremos o código de todos os microserviços, tudo pela linha de comando. E vamos fazer tudo funcionar perfeitamente!
+
+Escolhemos o GitHub para hospedarmos nosso código, a maior plataforma git do mundo. E você sabia que lá estão reunidos TODOS os projetos das ferramentas que usaremos neste curso? É incrível, né?
+
+Código rodando e código no GitHUb. Agora é implementar nossas aplicações em containers Docker. Vamos tornar tudo ainda mais prático e ágil.
       </>
     ),
   },
   {
-    title: 'Hands on do comeco ao fim',
-    Svg: require('@site/static/img/homepagefeatures/devcode.svg').default,
+    title: 'BUILD',
+    Svg: require('@site/static/img/homepagefeatures/build.svg').default,
     description: (
       <>
-        Aprenda fazendo! Colocaremos a mão a massa do inicio ao fim do curso e em praticamente todas as aulas. Há bastante trabalho a ser feito e muitas
-        ferramentas a serem implementadas, para concluirmos com sucesso a nossa missão
+        Precisamos empacotar o nosso código em containers e utilizaremos o Docker para isso!
+
+Vamos criar arquivos `Dockerfile` para cada um dos microserviços. E então `docker build` para gerar nas nossas imagens.
+
+Images criadas, vamos rodar elas em conjunto e garantir que elas estejam se comunicando.
+
+Maravilha! Conseguimos, agora vamos enviar nossas images pro docker hub e concluiremos mais um módulo!
+
+Ah e com isso já estamos prontos para dar o próximo passo e colocar a Gataria no Kubernetes 🕸️!
       </>
     ),
   },
   {
-    title: 'Containers e Kubernetes',
-    Svg: require('@site/static/img/homepagefeatures/containers.svg').default,
+    title: 'TESTE',
+    Svg: require('@site/static/img/homepagefeatures/test.svg').default,
     description: (
       <>
-        Implantaremos tudo com containers! A construção das nossas imagens será feita com Docker. Utilizaremos o Kubernetes
-        pare manter nossos ambientes operando, escalar nossa aplicações quando necessário e dividir os ambientes de produção e desenvolvimento
+        Automação é a palavra-chave para o sucesso do código da gataria!
+
+Percebemos que os desenvolvedores já criaram alguns testes e estavam executando-os manualmente.
+
+Queremos garantir a qualidade do nosso trabalho, por isso, estamos introduzindo testes automatizados em nossas pipelines CI/CD. Isso significa que toda vez que novos códigos forem adicionados às nossas branchs com CI/CD, os testes serão executados automaticamente, garantindo que tudo funcione perfeitamente!
       </>
     ),
   },
   {
-    title: 'Infraestrutra orientada ao Git',
-    Svg: require('@site/static/img/homepagefeatures/gitflow.svg').default,
+    title: 'Release',
+    Svg: require('@site/static/img/homepagefeatures/release.svg').default,
     description: (
       <>
-        O GitHub será o nosso versionador de código. Implantaremos uma estratégia de Git Flow real. 
-        E a nossa infraestrutura irá caminhar junto com o git utilizando o Github Actions
+        Chegou o momento especial em que nosso código está pronto para ir para produção! Finalmente, os esforços do desenvolvedor ganharão vida e estarão disponíveis para todos os nossos usuários.
+
+E a nossa pipeline CI/CD precisa de um passo a mais quando vamos para produção! O Release!
+
+Mas primeiro vamos definir uma estrategia de Git Flow real! Afinal a nossa infraestrutura vai caminhar junto com o git!
+
+Agora, vamos entender o que é versionamento semântico! E a gente passa a ver o SemVer em todo lugar depois. E melhor vamos colocar para que as trocas das versões também sejam feitas de maneira automática!
+
+Com essas duas estratégias sólidas definidas, estamos prestes a criar um Release incrível diretamente no Github! E o melhor de tudo: automatizado! 
       </>
     ),
   },
   {
-    title: 'Automatizando tudo com pipelines CI/CD',
-    Svg: require('@site/static/img/homepagefeatures/cicd.svg').default,
+    title: 'DEPLOY',
+    Svg: require('@site/static/img/homepagefeatures/deploy.svg').default,
     description: (
       <>
-        Do Build ao Deploy, criaremos do zero as Pipelines CI/CD. 
-        Todos os passos serão feitos de maneira automatizada pelo Github Actions, e os runners serão executados dentro do nosso cluster pois serão
-        do tipo Self-hosted
+É hora do deploy! Chegou o momento de trocar a imagem dos containers antigos pela nova, tanto no ambiente de desenvolvimento quanto na produção!
+
+Nada de preocupações agora, pois com o Kubernetes, nosso deploy será feito sem Downtime, garantindo uma transição suave e fluida.
+
+E a estrela do show é o Helm! Além de cuidar dos nossos deploys, o Helm também é um capaz de fazer rollbacks! Nessa área, já ouviu falar que mais importante que fazer deploys rápido, é fazer rollbacks rápidos? Caso algo de errado! E o Helm esta aqui para isto, deploys e rollbacks.
+
+Também vamos criar Helm charts próprios para as apps da Gataria
+
+Mas a melhor parte é que não faremos o deploy manualmente, teremos um processo totalmente automatizado! É aqui que entra o Github Actions, nossa plataforma de CI/CD. Durante o Bootcamp, construiremos nossas pipelines do zero!
+
+Então, resumindo: o Helm será responsável pelos deploys e rollbacks, enquanto o Github Actions automatizará tudo o que for possível! 
       </>
     ),
   },
   {
-    title: 'Monitoramento e Observabilidade',
-    Svg: require('@site/static/img/homepagefeatures/monitoring.svg').default,
+    title: 'OPERATE',
+    Svg: require('@site/static/img/homepagefeatures/operate.svg').default,
     description: (
       <>
-        Acompanhe tudo em um ambiente de monitoramento e observabilidade em tempo real com Grafana, Prometheus, Loki e que incluí até mesmo um APM
-         (Application Performance Monitoring) o SigNoz
+       Vamos levar a Gataria para o mundo do Kubernetes! 
+
+Com o poder do Kubernetes, iremos segmentar nossos ambientes em Dev e Prod usando namespaces. Os devs poderão visualizar as alterações no código no ambiente Dev, que será semelhante ao ambiente de Produção. Assim, ficarão mais tranquilos ao realizar o deploy na produção.
+
+Além disso, vamos aprender a configurar o HPA (Horizontal Pod Autoscaler) para nossas aplicações em produção. Com essa configuração, nossa aplicação ganhará escalabilidade automática para lidar com um alto volume de acessos e desescalar quando a demanda diminuir. Dessa forma, a gataria usará somente os recursos necessários.
+
+Iniciaremos criando nossos manifestos .yml manualmente no Kubernetes para um aprendizado mais sólido. Mas, logo em seguida, vamos adotar o Helm! Essa ferramenta tornará nossa vida muito mais fácil com o Kubernetes, facilitando o gerenciamento e a implantação de recursos.
+
+Acreditamos que o conteúdo sobre Kubernetes que estamos abordando é excelente para o aprendizado. A partir do 3º módulo, será apenas Kubernetes
+      </>
+    ),
+  },
+  {
+    title: 'MONITOR',
+    Svg: require('@site/static/img/homepagefeatures/monitor.svg').default,
+    description: (
+      <>
+        Monitoramento moderno é que propomos a ensinar vocês!
+
+E para isto vamos seguir 2 conceitos:
+
+4 Sinais de ouro da matéria do Google SRE (Site Reliability Engineering) e os 3 Pilares da Observabilidade.
+
+Para alcançar nossos objetivos, precisaremos utilizar algumas ferramentas. Começaremos com a já consolidada stack de DevOps (Prometheus, Grafana e Loki), e em seguida, adotaremos Tracings. É aí que entra o Open Telemetry! Vamos aprender a instrumentar o código da nossa Gataria usando bibliotecas de Auto Instrumentação e para a parte visual, contaremos com o poderoso SigNoz.
+
+O SigNoz é um APM (Application Performance Monitoring) e representa um conceito avançado em monitoramento! Mas não se preocupe, teremos uma aula sobre o que é um APM, para que todos fiquem por dentro.
+
+Você sabia que o Open Telemetry é capaz de coletar Métricas, Tracings e Logs? Isso proporciona uma plataforma única e completa de monitoramento! Mas vamos deixar esse assunto para o curso, que será incrível! 
+      </>
+    ),
+  },
+  {
+    title: 'PLAN',
+    Svg: require('@site/static/img/homepagefeatures/plan.svg').default,
+    description: (
+      <>
+       E chegamos na nossa última etapa: Plan.
+
+Já falamos que quando o código chega a produção é um momento especial, né? E vocês conhecem o conceito do CHANGELOG? Precisamos saber o que foi implementado, saber rastrear qual código subiu exatamente na nova versão. E faremos isso com o RELEASE do GitHub. Definimos na nossa estratégia de Git Flow que a branch de produção só aceita código via Pull Request, e também há as issues, com as descrições do que os devs deveriam fazer.
+
+Assim, nessa etapa, vamos automatizar esse rastreio! Após lançarmos o release, além da troca do número do nosso SemVer, será criado um release no GitHub com os Pull Requests e Tickets atrelados a ele que subiram nessa versão. 
+
+E como todo time bom tem uma documentação técnica boa! Apresentamos o Docusaurus para ser a documentação da Gataria. E assim, esse carinha tem um lugar especial no nosso coração. 
+
+Você sabia que o código da rawdevops.io é feito com Docusaurus? E melhor ainda, nosso código é aberto \o/ E caso queiram conferir, é só acessarem o projeto no GitHub: github.com/rawdevops/rawdevops.io 
       </>
     ),
   },
@@ -79,8 +153,8 @@ function Feature({ title, Svg, description }: FeatureItem) {
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+      <div className="text--left padding-horiz--md">
+        <h3 className="text--center">{title}</h3>
         <p>{description}</p>
       </div>
     </div>
